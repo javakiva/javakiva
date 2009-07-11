8d8c196e7221065e59ca584e50ea865178af6b5b
iprash <iprash@98128830-68ea-11de-a9b1-77b8bb241e6b>
2009-07-11 08:30:23 +0000

    Added functionality to get newest lenders and to search for lenders.

--- javakiva/src/com/prashsoft/javakiva/Lender.java
+++ javakiva/src/com/prashsoft/javakiva/Lender.java
@@ -6,13 +6,13 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * version 2 as published by the Free Software Foundation:
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * http://www.gnu.org/licenses/gpl-2.0.html
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License version 2 for more details:
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * http://www.gnu.org/licenses/gpl-2.0.html
  * 
  * You should have received a copy of the GNU General Public License
  * version 2 along with this program; if not, write to the
@@ -306,8 +306,12 @@ public class Lender {
      */
     @Override
     public String toString() {
-        return "" + " Lender Id: " + lenderId;
-
+        return "\nCountry Code: " + countryCode + "\nInvitee Count: " + inviteeCount
+                + "\nLender Id: " + lenderId + "\nLoan Because: " + loanBecause
+                + "\nLoan Count: " + loanCount + "\nMember Since: "
+                + memberSince + "\nName: " + name + "\nOccupational Info: "
+                + occupationalInfo + "\nPersonal Url: " + personalUrl
+                + "\nWhereabouts: " + whereabouts;
     }
 
 }
